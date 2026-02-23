64a5b5ee52e1a576acb5c17b6e273b642932a9d8
Divyanshu <divyanshu.jangid9891@gmail.com>
2026-02-24 01:38:27 +0530

    FEAT: tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,33 +1,42 @@
-import express from 'express';
+import express from "express";
 // const express = require('express');
 // import {ENV} from './lib/env.js';
-import path from "path"
- import dotenv from 'dotenv';
+import path from "path";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
- dotenv.config();
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Recuriter backend API!' });
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URL);
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Recuriter backend API!" });
 });
-app.get('/books', (req, res) => {
+app.get("/books", (req, res) => {
   res.status(200).json({ message: "Books endpoint is working!" });
 });
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend','dist','index.html'));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async() => {
   console.log(`Server is running on port ${process.env.PORT}`);
+  await connectDB();
 });
-
-
-
